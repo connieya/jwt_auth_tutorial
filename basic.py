022480d39f68dc0120d66caee994db4d826604d7
connieya <gunny6026@naver.com>
2022-12-27 11:33:33 +0900

    [feat] : refresh 토큰 생성 및 검증

--- basic.py
+++ basic.py
@@ -40,7 +40,8 @@ def login(user: User, Authorize: AuthJWT = Depends()):
 
     # subject identifier for who this token is for example id or username from database
     access_token = Authorize.create_access_token(subject=user.username)
-    return { "access_token": access_token}
+    refresh_token = Authorize.create_refresh_token(subject=user.username)
+    return { "access_token": access_token, "refresh_token": refresh_token}
 
 
 # protect endpoint with function jwt_required(), which requires
@@ -53,8 +54,27 @@ def user(Authorize: AuthJWT = Depends()):
     return { "user": current_user}
 
 
+@app.post('/refresh')
+def refresh(Authorize: AuthJWT = Depends()):
+    """
+    The jwt_refresh_token_required() function insures a valid refresh
+    token is present in the request before running any code below that function.
+    we can use the get_jwt_subject() function to get the subject of the refresh
+    token, and use the create_access_token() function again to make a new access token
+    """
+    Authorize.jwt_refresh_token_required()
+    # 헤더에 access_token 을 실어서 요청하면
+    # 에러 발생 => {"detail": "Only refresh tokens are allowed" }
+
+    current_user = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user)
+    return { "access_token": new_access_token}
+
+
 @app.get('/partially-protected')
 def partially_protected(Authorize: AuthJWT = Depends()):
+    # In some cases you want to use one endpoint for both,
+    #  protected and unprotected. In this situation you can use function jwt_optional().
     Authorize.jwt_optional()
     # If no jwt is sent in the request, get_jwt_subject() will return None
     current_user = Authorize.get_jwt_subject() or "anonymous"
